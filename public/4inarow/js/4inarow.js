a031bdc6e1b16da39bc23698b5627068347eb188
Cory Metcalfe <corymetcalfe@gmail.com>
2020-03-28 20:20:20 -0500

    check for win logic, needs testing

--- public/4inarow/js/4inarow.js
+++ public/4inarow/js/4inarow.js
@@ -12,12 +12,13 @@ document.addEventListener('DOMContentLoaded', function() {
 }, false);
 
 
+let blankSpace = '⚪';
 let turn = '🔴';
 function restart() {
   var cells = document.querySelectorAll("table#grid td");
   let i;
   for (i=0; i<cells.length; i++) {
-    cells[i].innerHTML = '⚪';
+    cells[i].innerHTML = blankSpace;
   }
 
   setDropIcons(turn);
@@ -34,15 +35,18 @@ function dropPress(col) {
   let boardRows = document.getElementById('grid').rows;
   let row;
   for (row=boardRows.length-1; row>=0; row--) {
-    if (boardRows[row].cells[col].innerHTML === '⚪') {
+    if (boardRows[row].cells[col].innerHTML === blankSpace) {
       boardRows[row].cells[col].innerHTML = turn;
-      console.log("placed row " + row);
+      console.log("placed (" + col + "," + row + ")");
       break;
     }
   }
-  if (boardRows[0].cells[col].innerHTML != '⚪') {
+  if (boardRows[0].cells[col].innerHTML != blankSpace) {
     document.getElementById("drop"+col).style.visibility = "hidden";
   }
+
+  checkForWin();
+
   turn = turn === '🔴' ? '🔵' : '🔴';
   setDropIcons(turn);
 }
@@ -54,3 +58,63 @@ function setDropIcons(icon) {
     dropIcons[j].innerHTML = icon;
   }
 }
+
+function checkForWin() {
+  let win = false;
+  let boardRows = document.getElementById('grid').rows;
+  let row, col;
+  for (row=0; row<boardRows.length; row++) {
+    if (win) break;
+    for (col=0; col<boardRows[row].cells.length; col++) {
+      let currentVal = boardRows[row].cells[col].innerHTML;
+      if (currentVal === blankSpace) {
+        break;
+      }
+      //check across, to the right
+      if (!win && col < 4) {
+        if (currentVal === boardRows[row].cells[col+1].innerHTML
+              && currentVal === boardRows[row].cells[col+2].innerHTML
+              && currentVal === boardRows[row].cells[col+3].innerHTML
+          ) {
+          win = true;
+        }
+      }
+      //check down
+      if (!win && row < 3) {
+        if (currentVal === boardRows[row+1].cells[col].innerHTML
+              && currentVal === boardRows[row+2].cells[col].innerHTML
+              && currentVal === boardRows[row+3].cells[col].innerHTML
+          ) {
+          win = true;
+        }
+      }
+      //check diagonal, down to the right
+      if (!win && col < 4 && row < 3) {
+        if (currentVal === boardRows[row+1].cells[col+1].innerHTML
+              && currentVal === boardRows[row+2].cells[col+2].innerHTML
+              && currentVal === boardRows[row+3].cells[col+3].innerHTML
+          ) {
+          win = true;
+        }
+      }
+      //check diagonal, up to the right
+      if (!win && col < 4 && row > 2) {
+        if (currentVal === boardRows[row-1].cells[col+1].innerHTML
+              && currentVal === boardRows[row-2].cells[col+2].innerHTML
+              && currentVal === boardRows[row-3].cells[col+3].innerHTML
+          ) {
+          win = true;
+        }
+      }
+    }
+  }
+  if (win) {
+    let buttons = document.querySelectorAll("div .drop");
+    let k;
+    for (k=0; k<buttons.length; k++) {
+      buttons[k].style.visibility = "hidden";
+    }
+    console.log("winner");
+  }
+}
+
