aeb3803aa92dd1242f4e16858663a2fba7cbf022
coryallegory <corymetcalfe@gmail.com>
2020-04-01 01:13:19 +0000

    Deploying to gh-pages from  @ 84fa0b8d15d664259e95c22a0a8595d24db48c3c 🚀

--- 4inarow/js/4inarow.js
+++ 4inarow/js/4inarow.js
@@ -1,17 +1,4 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-  function updateScale() {
-    var layoutHeight = document.getElementById('layout').offsetHeight;
-    var newScale = document.body.offsetHeight / layoutHeight;
-    var scale = 'scale(' + newScale + ',' +  newScale + ')';
-    document.getElementById('layout').style.transform = scale;
-  }
-  window.onresize = updateScale;
-  updateScale();
-  restart();
-}, false);
-
-
 let blankSpace = '⚪';
 let player = '🔴';
 let human = '🔵';
--- splat/js/splat.js
+++ splat/js/splat.js
@@ -1,17 +1,6 @@
 var number;
 var count;
 
-document.addEventListener('DOMContentLoaded', function() {
-  function updateScale() {
-    var layoutHeight = document.getElementById('layout').offsetHeight;
-    var newScale = document.body.offsetHeight / layoutHeight;
-    var scale = 'scale(' + newScale + ',' +  newScale + ')';
-    document.getElementById('layout').style.transform = scale;
-  }
-  window.onresize = updateScale;
-  updateScale();
-  restart();
-}, false);
 
 function restart() {
   number = Math.floor(Math.random() * 100) + 1;
